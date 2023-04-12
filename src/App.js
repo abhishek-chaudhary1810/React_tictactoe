82787c0f87a3119a1488cfb25c11aab8cfa338b3
Abhishek Chaudhary <abhishekchoudharuy1810@gmail.com>
2023-04-12 06:49:00 +0530

    completed UI for tiktaktoe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function Box({ value }) {
+  function handleClick() {
+    console.log("Clicked");
+  }
   return (
-    <div >
-      <h1>Hey There!</h1>
-    </div>
+    <button className="buttonStyle" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Box value="1" />
+        <Box value="2" />
+        <Box value="3" />
+      </div>
+      <div className="board-row">
+        <Box value="4" />
+        <Box value="5" />
+        <Box value="6" />
+      </div>
+      <div className="board-row">
+        <Box value="7" />
+        <Box value="8" />
+        <Box value="9" />
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Board;
