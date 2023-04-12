51585a897476cc2ca71dc4ba127bbffcc81db31b
Abhishek Chaudhary <abhishekchoudharuy1810@gmail.com>
2023-04-12 16:54:15 +0530

    Feature:Time Travel - In Progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Board  from './Components/Board';
+import Game  from './Components/Board';
 function App() {
   return (
     <div >
       <h1>Hey There!</h1>
-      <Board />
+      <Game />
     </div>
   );
 }
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,4 +1,32 @@
 import { useState } from "react";
+//top component
+export default function Game() {
+  const [xIsNext, xSetNext] = useState(true);
+  //array of 9 null element in a state initially
+  const [moveHistory, setMoveHistory] = useState([Array(9).fill(null)]);
+
+  const currentSquares = moveHistory[moveHistory.length - 1];
+  //nextboxes is prop we could use here as we have lifted state up
+  function handlePlay(nextBoxes) {
+    //appending a state into already existed state
+    setMoveHistory([...moveHistory, nextBoxes]);
+    //next user turn is determined by setnext as after a move its boolean is flipped
+    xSetNext(!xIsNext);
+  }
+  return (
+    <>
+      <div>
+        {/* Board component is lifted up to Game component and passed 3 props*/}
+        <Board xIsNext={xIsNext} boxes={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div>
+        {/* ordered list in a div */}
+        <ol>{}</ol>
+      </div>
+    </>
+  );
+}
+//most low level component
 function Box({ value, onBoxClick }) {
   return (
     <button className="buttonStyle" onClick={onBoxClick}>
@@ -6,71 +34,69 @@ function Box({ value, onBoxClick }) {
     </button>
   );
 }
-export default function Board() {
-  const [xisnext,xSetisnext]=useState(true);
-  const [boxes, setboxes] = useState(Array(9).fill(null));
-  //console.log(boxes)
+function Board({ xIsNext, boxes, onPlay }) {
   function handleClick(i) {
-   // console.log(boxes[i]);
-   if(calculateWinner(boxes) || boxes[i]){
-    return;
-   }
+    //checked initial and winner condition
+    if (calculateWinner(boxes) || boxes[i]) {
+      return;
+    }
+    //slice is used to make a copy of array rather than mutating it
     const nextBoxes = boxes.slice();
-    if (xisnext===true){
+    if (xIsNext === true) {
       nextBoxes[i] = "X";
+    } else {
+      nextBoxes[i] = "O";
     }
-    else{
-      nextBoxes[i]="O";
-    }
-    setboxes(nextBoxes);
-    xSetisnext(!xisnext);
+    onPlay(nextBoxes);
   }
-  const winner=calculateWinner(boxes);
-  
-  let status = xisnext ? "X" : "O";
+  //declared and store 2 value to show in jsx in Board component
+
+  const winner = calculateWinner(boxes);
+
+  let status = xIsNext ? "X" : "O";
   return (
     <>
-    <div>
-      <h1>Next Player Turn :{status}</h1>
-    </div>
-    <div>
-      <h1>winner is :{winner}
-      </h1> </div>
-      <div className="board-row">
-        <Box value={boxes[0]} onBoxClick={()=>handleClick(0)} />
-        <Box value={boxes[1]} onBoxClick={()=>handleClick(1)} />
-        <Box value={boxes[2]} onBoxClick= {()=>handleClick(2)}/>
+      <div>
+        <h1>Next Player Turn :{status}</h1>
+      </div>
+      <div>
+        <h1>winner is :{winner}</h1>{" "}
       </div>
       <div className="board-row">
-        <Box value={boxes[3]} onBoxClick={()=>handleClick(3)} />
-        <Box value={boxes[4]} onBoxClick={()=>handleClick(4)} />
-        <Box value={boxes[5]} onBoxClick={()=>handleClick(5)} />
+        <Box value={boxes[0]} onBoxClick={() => handleClick(0)} />
+        <Box value={boxes[1]} onBoxClick={() => handleClick(1)} />
+        <Box value={boxes[2]} onBoxClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Box value={boxes[6]} onBoxClick={()=>handleClick(6)} />
-        <Box value={boxes[7]} onBoxClick={()=>handleClick(7)} />
-        <Box value={boxes[8]} onBoxClick={()=>handleClick(8)} />
+        <Box value={boxes[3]} onBoxClick={() => handleClick(3)} />
+        <Box value={boxes[4]} onBoxClick={() => handleClick(4)} />
+        <Box value={boxes[5]} onBoxClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Box value={boxes[6]} onBoxClick={() => handleClick(6)} />
+        <Box value={boxes[7]} onBoxClick={() => handleClick(7)} />
+        <Box value={boxes[8]} onBoxClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
-function calculateWinner(boxes){
-  const lines =[
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
+//javascript function to check if tiktaktoe is completed either by "X" or "O"
+//boxes is passed as a prop inside Board component during this calculate winner function call
+function calculateWinner(boxes) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-  for (let i=0;i<lines.length;i++)
-  {
-    const [a,b,c]=lines[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
     if (boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
       return boxes[a];
     }
-
   }
 }
